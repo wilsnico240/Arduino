612e8fa75270b21baa3d9cfa4fde50fd6346ccae
nico <85696452+wilsnico240@users.noreply.github.com>
2023-07-02 10:50:15 +0200

    greenhouse.ino

--- greenhouse.ino
+++ greenhouse.ino
@@ -1,3 +1,4 @@
+
 #include <DHT.h>
 
 #define DHTPIN 4
@@ -6,42 +7,62 @@ DHT dht(DHTPIN, DHTTYPE);
 
 #define RELAY1 5
 #define RELAY2 6
+#define RELAY3 2
+#define RELAY4 3
 #define ON  LOW
 #define OFF HIGH
 
+void(* resetFunc) (void) = 0;
 unsigned long startTime;
+unsigned long startTime2;
+unsigned long startTime3;
 unsigned long currentTime;
-
+unsigned long currentTime2;
+unsigned long currentTime3;
 void setup() {
-  Serial.begin(9600);
-  pinMode(RELAY1, OUTPUT);
-  pinMode(RELAY2, OUTPUT);
-  digitalWrite(RELAY1, OFF);
-  digitalWrite(RELAY2, OFF);
-  dht.begin();
-  startTime = millis();
+   Serial.begin(9600);
+   pinMode(RELAY1, OUTPUT);
+   pinMode(RELAY2, OUTPUT);
+   pinMode(RELAY3, OUTPUT);
+   pinMode(RELAY4, OUTPUT);
+   digitalWrite(RELAY1, OFF);
+   digitalWrite(RELAY2, OFF);
+   digitalWrite(RELAY3, ON);
+   digitalWrite(RELAY4, ON);
+   dht.begin();
+   startTime = millis();
+   startTime2 = millis();
+   startTime3 = millis();
 }
 
 void loop() {
-  currentTime = millis();
-  int temperature = dht.readTemperature();
-  int humidity = dht.readHumidity();
-  if (currentTime - startTime >= 300000) {
+   currentTime = millis();
+   currentTime2 = millis();
+   currentTime3 = millis();
+   int temperature = dht.readTemperature();
+   int humidity = dht.readHumidity();
+   
+    if (currentTime2 - startTime2 >= 64800000) {
+    startTime2 = currentTime2;
+      digitalWrite(RELAY3, OFF);
+      digitalWrite(RELAY4, OFF);
+      }
+    if (currentTime3 - startTime3 >= 86400000) {
+    startTime3 = currentTime3;
+    resetFunc();
+}
+    if (currentTime - startTime >= 300000) {
     startTime = currentTime;
-    if (temperature >= 30) {
+    if (temperature >= 28) {
       digitalWrite(RELAY1, ON);
-      Serial.println("Temperatuur is te hoog!");
     }
-    else if (temperature <= 30) {
+    else if (temperature <= 28) {
       digitalWrite(RELAY1, OFF);
-      Serial.println("Temperatuur is in orde.");
     }
-    if (humidity <= 40) {
+    if (humidity <= 50) {
       digitalWrite(RELAY2, ON);
-      Serial.println("Luchtvochtigheid is te laag!");
-      delay(180000);
+      delay(30000);
       digitalWrite(RELAY2, OFF);
-      Serial.println("Luchtvochtigheid is in orde.");
     }
   }
   Serial.print("Temperatuur: ");
@@ -64,5 +85,21 @@ void loop() {
   else {
     Serial.println("uit.");
   }
+  Serial.print("Relais 3 staat ");
+  if (digitalRead(RELAY3) == ON) {
+    Serial.println("aan.");
+  }
+  else {
+    Serial.println("uit.");
+  }
+
+  Serial.print("Relais 4 staat ");
+  if (digitalRead(RELAY4) == ON) {
+    Serial.println("aan.");
+  }
+  else {
+    Serial.println("uit.");
+  }
+
   Serial.println(" ");
 }
