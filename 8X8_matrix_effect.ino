6b4871da3541a9150457fb6e024f95c30a9a206e
nico <85696452+wilsnico240@users.noreply.github.com>
2025-02-01 16:10:41 +0100

    Add files via upload

---
+++ 8X8_matrix_effect.ino
@@ -0,0 +1,51 @@
+#include "Adafruit_NeoPixel.h"
+#define WS2812b_PIN 12
+#define WS2812b_PIXELS 64
+#define WS2812b_WIDTH 8
+#define WS2812b_HEIGHT 8
+Adafruit_NeoPixel WS2812b = Adafruit_NeoPixel(WS2812b_PIXELS, WS2812b_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+Serial.begin(9600);
+WS2812b.begin();
+WS2812b.clear();
+WS2812b.show();
+}
+
+void matrixEffect() {
+WS2812b.setBrightness(100);
+static int columnHeads[WS2812b_WIDTH] = {-1}; 
+const uint8_t spawnChance = 30; 
+const uint32_t groen = WS2812b.Color(0, 150, 0);
+for(int i=0; i<WS2812b.numPixels(); i++) {
+uint32_t kleur = WS2812b.getPixelColor(i);
+uint8_t r = (kleur >> 16) & 0xFF;
+uint8_t g = (kleur >> 8) & 0xFF;
+uint8_t b = kleur & 0xFF;
+WS2812b.setPixelColor(i, r*0.7, g*0.7, b*0.7);
+}
+for(int col=0; col<WS2812b_WIDTH; col++) {
+if(columnHeads[col] == -1) {
+if(random(255) < spawnChance) {
+columnHeads[col] = 0; // Start bovenaan
+}
+} 
+else {
+int row = columnHeads[col];
+int pixelIndex = (row % 2 == 0) ? 
+row * WS2812b_WIDTH + col : 
+row * WS2812b_WIDTH + (WS2812b_WIDTH - 1 - col);
+WS2812b.setPixelColor(pixelIndex, groen);
+if(row > 0) WS2812b.setPixelColor(pixelIndex - WS2812b_WIDTH, groen/2);
+if(row > 1) WS2812b.setPixelColor(pixelIndex - 2*WS2812b_WIDTH, groen/4);
+columnHeads[col]++; 
+if(columnHeads[col] >= WS2812b_HEIGHT + 2) {
+columnHeads[col] = -1;
+}}}
+WS2812b.show();
+delay(75);
+}
+
+void loop() {
+matrixEffect();
+}
